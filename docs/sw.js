8e957f0cc53438d00714923624b8e79cf43da16d
dimo <forker4@dim0.uu.me>
2023-04-05 00:19:28 +0800

    230404

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/dl8z/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230404.224503';
+const cacheName = 'chirpy-20230405.001909';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
